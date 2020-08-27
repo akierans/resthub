a5cd3e2201342a2ebe00e5c2387771536c864617
akierans <akierans@gmail.com>
2020-08-27 17:52:14 +0100

    added methods for updating contacts

--- api-routes.js
+++ api-routes.js
@@ -1,14 +1,23 @@
-//Initialise express router
+// api-routes.js
+// Initialize express router
 let router = require('express').Router();
-
-//Set default API response
-router.get('/', function(req, res) {
-	res.json({
-		status: 'API It\'s Working',
-		message: 'Welcome to RESTHub crafted with love!'
-	});
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to RESTHub crafted with love!',
+    });
 });
-
-
-//Export API Routes
+// Import contact controller
+var contactController = require('./contactController');
+// Contact routes
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+router.route('/contacts/:contact_id')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
+// Export API routes
 module.exports = router;
